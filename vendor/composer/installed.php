34d4f19e1113f1a90a5b5ac6a00b308eb14cf3bc
Ian Surii <ianmuthuri254@gmail.com>
2022-02-08 11:33:37 +0300

    initial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f795bff47ede1766f131e7c8b62a2d370065b6e9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f795bff47ede1766f131e7c8b62a2d370065b6e9',
+            'dev_requirement' => false,
+        ),
+        'symfony/dotenv' => array(
+            'pretty_version' => 'v6.0.3',
+            'version' => '6.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/dotenv',
+            'aliases' => array(),
+            'reference' => '45c47b67cc92fbcf9b1d40f08e970a136f75559f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
